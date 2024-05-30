49fb78aadbbb36c2d154da85bff018d52d5e12ea
Jason Zhu <55606978+jasonmeme@users.noreply.github.com>
2024-05-30 14:29:14 -0400

    fixed structure

--- app.py
+++ app.py
@@ -1,27 +1,10 @@
-# api/app.py
-
 from flask import Flask, request, render_template, url_for
 import smtplib
 from email.mime.text import MIMEText
 import uuid
 import sqlite3
-import requests
-import os
 
 app = Flask(__name__)
-
-DATABASE = os.path.join(os.getcwd(), 'phishing.db')
-
-def init_db():
-    if not os.path.exists(DATABASE):
-        conn = sqlite3.connect(DATABASE)
-        c = conn.cursor()
-        c.execute('''CREATE TABLE IF NOT EXISTS clicks
-                    (id TEXT PRIMARY KEY, email TEXT, clicked BOOLEAN)''')
-        conn.commit()
-        conn.close()
-
-init_db()
 
 @app.route('/')
 def index():
@@ -33,19 +16,15 @@ def send_phishing_email():
     unique_id = str(uuid.uuid4())
     tracking_url = url_for('track_click', id=unique_id, _external=True)
     
-    # Shorten the URL using TinyURL API
-    response = requests.get(f'http://tinyurl.com/api-create.php?url={tracking_url}')
-    shortened_url = response.text
-    
     # Store tracking information in the database
-    conn = sqlite3.connect(DATABASE)
+    conn = sqlite3.connect('phishing.db')
     c = conn.cursor()
     c.execute('''INSERT INTO clicks (id, email, clicked) VALUES (?, ?, ?)''', (unique_id, email, False))
     conn.commit()
     conn.close()
 
     subject = 'Test Phishing Email'
-    body = f'This is a simulated phishing email. Do not click on any links.\nTracking URL: {shortened_url}'
+    body = f'This is a simulated phishing email. Do not click on any links.\nTracking URL: {tracking_url}'
     
     msg = MIMEText(body)
     msg['Subject'] = subject
@@ -69,7 +48,7 @@ def send_phishing_email():
 @app.route('/track/<id>')
 def track_click(id):
     # Update database to indicate the link was clicked
-    conn = sqlite3.connect(DATABASE)
+    conn = sqlite3.connect('phishing.db')
     c = conn.cursor()
     c.execute('''UPDATE clicks SET clicked = ? WHERE id = ?''', (True, id))
     conn.commit()
@@ -79,7 +58,7 @@ def track_click(id):
 
 @app.route('/results')
 def view_results():
-    conn = sqlite3.connect(DATABASE)
+    conn = sqlite3.connect('phishing.db')
     c = conn.cursor()
     c.execute('''SELECT * FROM clicks''')
     rows = c.fetchall()
