7b8029c297ccfe116d547a31cdec8f63ac06453a
Jason Zhu <55606978+jasonmeme@users.noreply.github.com>
2024-06-03 09:29:18 -0400

    Update app.py

--- api/app.py
+++ api/app.py
@@ -79,12 +79,11 @@ def send_phishing_email():
                     <h1>Verify Your Account</h1>
                 </div>
                 <div class="email-body">
-                    <p>Dear Hi,</p>
+                    <p>Dear {email},</p>
                     <p>We noticed unusual login activity on your account, and for your security, we need you to verify your account information. Please take a moment to review your recent activity and ensure that your account information is up to date.</p>
                     <p>To verify your account, please click the button below:</p>
                     <p><a href="{tracking_url}" class="btn">Verify Your Account</a></p>
                     <p>If you did not attempt to log in, please disregard this email, and we apologize for any inconvenience.</p>
-                    <p>Thank you for your prompt attention to this matter.</p>
                     <p>Best regards,</p>
                     <p><br>Webex<br><a href="mailto:help.webex.cisco@gmail.com">help.webex.cisco@gmail.com</a></p>
                 </div>
